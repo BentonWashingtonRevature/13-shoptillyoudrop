48069d80121fe3d85171f44db51b763cfad6ef7b
Code 41 <washingtonbenton@gmail.com>
2022-08-09 21:28:04 -0400

    finished routes and most of assignment

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -4,19 +4,28 @@ const { Product, Category, Tag, ProductTag } = require('../../models');
 // The `/api/products` endpoint
 
 // get all products
-router.get('/', (req, res) => {
+router.get('/', async (req, res) => {
   // find all products
   // be sure to include its associated Category and Tag data
+  const productData = await Product.findAll({
+    include: [{ model: Category, Tag}],
+  }); 
+  return res.json(productData);
 });
 
 // get one product
-router.get('/:id', (req, res) => {
+router.get('/:id', async (req, res) => {
   // find a single product by its `id`
   // be sure to include its associated Category and Tag data
+  const productData = await Product.findByPk(req.params.id, {
+    include: [{ model: Category, Tag }]
+  });
+
+  return res.json(productData)
 });
 
 // create new product
-router.post('/', (req, res) => {
+router.post('/', async(req, res) => {
   /* req.body should look like this...
     {
       product_name: "Basketball",
@@ -25,7 +34,15 @@ router.post('/', (req, res) => {
       tagIds: [1, 2, 3, 4]
     }
   */
-  Product.create(req.body)
+ 
+  await Product.create(
+    {
+      product_name: "Basketball",
+      price: 200.00,
+      stock: 3,
+      tagIds: [1, 2, 3, 4]
+    }
+  )
     .then((product) => {
       // if there's product tags, we need to create pairings to bulk create in the ProductTag model
       if (req.body.tagIds.length) {
@@ -89,8 +106,17 @@ router.put('/:id', (req, res) => {
     });
 });
 
-router.delete('/:id', (req, res) => {
+router.delete('/:id', async (req, res) => {
   // delete one product by its `id` value
+  const tagData = await Tag.destroy({
+    where: {
+      id: req.params.id,
+    },
+  });
+
+  return res.json(tagData);
 });
+
+
 
 module.exports = router;
